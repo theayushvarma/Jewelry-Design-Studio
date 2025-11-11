7622bb51d5da1864238f0041075d8b66b4e546e5
Ayush Varma <vayush798@gmail.com>
2025-11-12 02:42:50 +0530

    chore: Update package dependencies for @heroui/react and @heroui/theme; add vercel configuration file for deployment

--- client/vite.config.ts
+++ client/vite.config.ts
@@ -2,14 +2,14 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import tsconfigPaths from "vite-tsconfig-paths";
 
-// Skip eslint plugin if you don’t want lint errors too
 export default defineConfig({
   plugins: [
     react(),
     tsconfigPaths()
   ],
-
-  // ✅ Suppress known esbuild type warnings
+  server: { port: 5173 },
+  preview: { port: 4173 },
+  base: "/",
   esbuild: {
     logOverride: { 'this-is-undefined-in-esm': 'silent' }
   }
